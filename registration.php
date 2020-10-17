897dacdedb3e1f14b6b1a45f14ba0e1b69e5b6e4
Solwininfotech <stdabhoya@yahoo.com>
2020-10-17 15:08:50 +0530

    initial commit

---
+++ registration.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Solwin Infotech
+ * Solwin GoogleCustomSearch Extension
+ *
+ * @category   Solwin
+ * @package    Solwin_GoogleCustomSearch
+ * @copyright  Copyright © 2006-2016 Solwin (https://www.solwininfotech.com)
+ * @license    https://www.solwininfotech.com/magento-extension-license/
+ * End-user License Agreement
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Solwin_GoogleCustomSearch',
+    __DIR__
+);
